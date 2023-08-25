86a1cecdd9950f4a398624f9716cf8ad6dcd6b36
Aditya Rajput <adiraj20072002@gmail.com>
2023-08-25 18:13:55 +0000

    feat: Initialize basic Slack app

--- server/main.py
+++ server/main.py
@@ -1,5 +1,25 @@
-def main():
-    print("main")
+import os
+from typing import Any
+
+from dotenv import load_dotenv
+from slack_bolt import App, Ack, Say
+
+
+load_dotenv()
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+)
+
+
+@app.event("app_mention")
+def on_mention(ack: Ack, say: Say, payload: dict[str, Any]):
+    ack()
+
+    ts = payload.get("thread_ts", None) or payload.get("ts", None)
+
+    say(text="Thanks for mentioning me!", thread_ts=ts)
+
 
 if __name__ == "__main__":
-    main()
+    app.start(port=int(os.environ.get("PORT", 3000)))
