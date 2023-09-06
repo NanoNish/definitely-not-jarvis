12547db99e553ac855fc6a7bdda571e7a27a4d95
Aditya Rajput <adiraj20072002@gmail.com>
2023-09-06 13:53:29 +0000

    feat: Classify

--- server/clarifai.py
+++ server/clarifai.py
@@ -2,7 +2,8 @@ from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_pb2, status_code_pb2
 
-from prompt import prompt
+from prompt import assist_prompt, classify_prompt
+
 
 class ClarifaiService:
     def __init__(
@@ -16,16 +17,17 @@ class ClarifaiService:
         self.stub = service_pb2_grpc.V2Stub(channel)
 
         self.metadata = (('authorization', f'Key {api_key}'),)
-        self.user_data = resources_pb2.UserAppIDSet(user_id=user_id, app_id=app_id)
+        self.user_data = resources_pb2.UserAppIDSet(
+            user_id=user_id, app_id=app_id)
         self.workflow_id = workflow_id
 
-    def predict(
+    def answer(
         self,
         user_input: str,
         faqs: list[tuple[str, str]],
         knowledge: list[str],
-    ):
-        raw_input = prompt(
+    ) -> str:
+        raw_input = assist_prompt(
             faqs=faqs,
             knowledge=knowledge,
             query=user_input,
@@ -41,7 +43,7 @@ class ClarifaiService:
             ),
         )
         request = service_pb2.PostWorkflowResultsRequest(
-            user_app_id=self.user_data,  
+            user_app_id=self.user_data,
             workflow_id=self.workflow_id,
             inputs=[input_to_model],
         )
@@ -53,9 +55,70 @@ class ClarifaiService:
 
         if response.status.code != status_code_pb2.SUCCESS:
             print(response.status)
-            raise Exception("Post workflow results failed, status: " 
+            raise Exception("Post workflow results failed, status: "
                             + response.status.description)
-        
+
         result = response.results[0].outputs[0]
 
         return result.data.text.raw
+
+    def classify(
+        self,
+        user_input: str,
+        knowledge: list[tuple[str, str]],
+    ) -> tuple[str, str]:
+        input1, input2 = classify_prompt(
+            knowledge=knowledge,
+            query=user_input,
+        )
+
+        print(input1, "\n", input2)
+
+        input_to_model1 = resources_pb2.Input(
+            data=resources_pb2.Data(
+                text=resources_pb2.Text(
+                    raw=input1,
+                ),
+            ),
+        )
+        input_to_model2 = resources_pb2.Input(
+            data=resources_pb2.Data(
+                text=resources_pb2.Text(
+                    raw=input2,
+                ),
+            ),
+        )
+
+        request1 = service_pb2.PostWorkflowResultsRequest(
+            user_app_id=self.user_data,
+            workflow_id=self.workflow_id,
+            inputs=[input_to_model1],
+        )
+        request2 = service_pb2.PostWorkflowResultsRequest(
+            user_app_id=self.user_data,
+            workflow_id=self.workflow_id,
+            inputs=[input_to_model2],
+        )
+
+        response1 = self.stub.PostWorkflowResults(
+            request1,
+            metadata=self.metadata,
+        )
+        response2 = self.stub.PostWorkflowResults(
+            request2,
+            metadata=self.metadata,
+        )
+
+        if [response1.status.code, response2.status.code] != [status_code_pb2.SUCCESS] * 2:
+            print(response1.status)
+            print(response2.status)
+            raise Exception("Post workflow results failed, status: "
+                            + response1.status.description
+                            + ", "
+                            + response2.status.description)
+
+        result1 = response1.results[0].outputs[0]
+        result2 = response2.results[0].outputs[0]
+
+        # type, project
+        return result1.data.text.raw, result2.data.text.raw
--- server/main.py
+++ server/main.py
@@ -3,7 +3,6 @@ from typing import Any
 
 from dotenv import load_dotenv
 from slack_bolt import App, Ack, Say
-
 from clarifai import ClarifaiService
 from blocks import make_blocks
 from strapi import get_all_faq, get_all_knowledge
@@ -39,59 +38,62 @@ BOT_ID = os.environ.get("SLACK_BOT_ID")
 faqs: list = get_all_faq()
 knowledge: list = get_all_knowledge()
 
-# Mentions
 @app.event("app_mention")
 def on_mention(ack: Ack, say: Say, payload: dict[str, Any]):
     ack()
 
     ts = payload.get("thread_ts", None) or payload.get("ts", None)
-    
+
     user_input = payload.get("text", "").replace(f"<@{BOT_ID}>", "").strip()
-    
-    response_text = assistant.predict(
+
+    response_text = assistant.answer(
         faqs=faqs,
         knowledge=knowledge,
         user_input=user_input,
     )
-    
+
     blocks = make_blocks(response_text, user_input)
-    
+
     say(blocks=blocks, text=response_text, thread_ts=ts)
+
 
 @app.action("try_again")
 def try_again(ack: Ack, say: Say, body: Any):
     ack()
-    
+
     ts = body.get("container", None).get("thread_ts", None)
-    
-    user_input = body.get("message", None).get("blocks", None)[1].get("elements", None)[0].get("value", None)
-    
-    response_text = assistant.predict(
+
+    user_input = body.get("message", None).get("blocks", None)[
+        1].get("elements", None)[0].get("value", None)
+
+    response_text = assistant.answer(
         faqs=faqs,
         knowledge=knowledge,
         user_input=user_input,
     )
-    
+
     blocks = make_blocks(response_text, user_input)
-    
+
     say(blocks=blocks, text=response_text, thread_ts=ts)
+
 
 @app.action("contact_human")
 def contact_human(ack: Ack, say: Say, body: Any):
     ack()
-    
+
     ts = body.get("container", None).get("thread_ts", None)
-    
-    user_input = body.get("message", None).get("blocks", None)[1].get("elements", None)[0].get("value", None)
-    
-    project, prompt_type = classifier.predict(
+
+    user_input = body.get("message", None).get("blocks", None)[
+        1].get("elements", None)[0].get("value", None)
+
+    input_type, project = classifier.classify(
         knowledge=knowledge,
         user_input=user_input,
     )
-    
+
     # TODO: get correct peeps
     # TODO: send message to correct peeps
-    print(project, prompt_type)
+    print(input_type, project)
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
